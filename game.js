1e3c209ac76439baed9cb376081bbf9242325e42
JTomoyasu <30738319+JTomoyasu@users.noreply.github.com>
2018-02-12 10:52:44 -0800

    f

---
+++ game.js
@@ -0,0 +1,13 @@
+function Enemy (hitp,caps)
+{
+    this.hp=hitp;
+    this.cap=caps;
+    //this.graphics=graphic;
+}
+function Player (hitp,att,caps)
+{
+    this.hp=hitp;
+    this.ap=att;
+    this.cap=caps;
+    //this.graphics=graphic;
+}
